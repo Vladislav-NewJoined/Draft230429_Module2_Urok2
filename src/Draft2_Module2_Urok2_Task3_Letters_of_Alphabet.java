2c876b932ca8054685399ed8cfd027fdbddde86f
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-04-30 10:23:53 +0300

    Initial commit

---
+++ src/Draft2_Module2_Urok2_Task3_Letters_of_Alphabet.java
@@ -0,0 +1,31 @@
+// Задание здесь: https://lms.synergy.ru/learning/view/68552/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
+// 3.	Выведите латинский алфавит от a до z
+public class Draft2_Module2_Urok2_Task3_Letters_of_Alphabet {
+    public static void main(String[] args) {
+        System.out.println("Задание: \n3.\tВыведите латинский алфавит от a до z\n");
+        System.out.println("Решение: ");
+
+//        for (char i = '\u0430'; i <= '\u044f'; i++) {
+//            System.out.print(i + " ");
+//        }
+
+//        for (char i = 'a'; i <= 'z'; i++) {
+//            System.out.print(i + " ");
+//        }
+
+        char i = 'a';
+        while (i <= 'z') {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        System.out.println();
+
+        char i2 = 'а';
+        while (i2 <= 'я') {
+            System.out.print(i2 + " ");
+            i2++;
+        }
+
+    }
+}
