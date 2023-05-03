5f9e64e3d02f832cb6579bd297c70a4a41ff348a
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-03 15:28:57 +0300

    Initial commit

--- src/Draft1_Whole_Video_While_1000_Files_etc.java
+++ src/Draft1_Whole_Video_While_1000_Files_etc.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Draft1_Whole_Video_While_1000_Files_etc {
     public static void main(String[] args) {
 
-//        1 код
+//        Код 1
 //        int x = 1;
-//        while (x < 11) {  // ввидео мин 02.15
+//        while (x < 11) {  // видео мин 02.15
 ////            File file = new File("C:\\Users\\PC\\IdeaProjectsDrafts\\Draft230429_Module2_Urok2\\files\\" + "File" + x);
 //            System.out.println("Loop can do a lot of things.");
 //            File file = new File("files\\" + "File" + x);
@@ -15,21 +15,21 @@ public class Draft1_Whole_Video_While_1000_Files_etc {
 //            x = x + 1;
 //        }
 
-//        2 код
+//        Код 2
 //        int x = 1;
 //        while (x < 6) {  // видео мин 11.59
 //            System.out.print("\t" + x);
 //            x++;
 //        }
 
-//        3 код
+//        Код 3
 //        int x = 100;
 //        while (x >= 50) {  // видео мин 11.59
 //            System.out.print("\t" + x);
 //            x--;
 //        }
 
-//        4 код
+//        Код 4
 //        int x = 1;
 //        while (x <= 50) {  // видео мин 15.35
 //            if (x % 2 == 0) {
@@ -56,12 +56,12 @@ public class Draft1_Whole_Video_While_1000_Files_etc {
             str = scanner.nextLine();
         } System.out.println("Ладно, достаточно");
 
-//        Код 6
+//        Код 7
 //        factorial здесь видео мин 28.09 https://lms.synergy.ru/learning/view/68494/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
 
 //        Resume Program Продолжить здесь видео мин 28.17 https://lms.synergy.ru/learning/view/68494/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
 
-//        Код 7
+//        Код 8
 //        Точка елочкой здесь видео мин 32.36 https://lms.synergy.ru/learning/view/68494/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
     }
 }
---
+++ src/URLConnection.java
@@ -0,0 +1,2 @@
+public class URLConnection {
+}
